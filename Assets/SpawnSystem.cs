53b75c54ac92fbfbdbb928f5a9c6410cedd38e3f
Jaden Balogh <jadenbalogh@gmail.com>
2022-12-11 17:51:32 -0500

    Update stuff

--- Assets/SpawnSystem.cs
+++ Assets/SpawnSystem.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class SpawnSystem : MonoBehaviour
 {
-    [SerializeField] private Enemy npcPrefab;
+    [SerializeField] private Enemy[] npcPrefabs;
     [SerializeField] private int npcCount = 10;
     [SerializeField] private Waypoint[] waypoints;
 
@@ -18,7 +18,7 @@ public class SpawnSystem : MonoBehaviour
         for (int i = 0; i < npcCount; i++)
         {
             Waypoint targetWaypoint = waypoints[Random.Range(0, waypoints.Length)];
-            Enemy enemy = Instantiate(npcPrefab, targetWaypoint.transform.position, Quaternion.identity);
+            Enemy enemy = Instantiate(npcPrefabs[Random.Range(0, npcPrefabs.Length)], targetWaypoint.transform.position, Quaternion.identity);
             enemy.AssignWaypoint(targetWaypoint.GetRandomNext());
 
             if (npcCount % 10 == 0)
